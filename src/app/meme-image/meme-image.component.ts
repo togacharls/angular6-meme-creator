c5722c53ed403c341749fa97fc901d8b4f687a62
Carlos Torres García <togacharls@gmail.com>
2018-07-12 16:24:18 +0200

    Subscribing to the HttpClient.get Observable and using callbacks, watching changes in MemeImageComponent

--- src/app/meme-image/meme-image.component.ts
+++ src/app/meme-image/meme-image.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {MemeService} from '../meme.service';
 
 @Component({
   selector: 'app-meme-image',
@@ -7,7 +8,29 @@ import {Component, Input} from '@angular/core';
 })
 export class MemeImageComponent {
 
-  @Input() imgSrc: String;
-  @Input() aboveText: String;
-  @Input() belowText: String;
+   _imgSrc: string;
+   _aboveText: string;
+   _belowText: string;
+   constructor(private memeService: MemeService) {}
+  @Input()
+  get imgSrc () {
+    return this._imgSrc;
+  }
+  set imgSrc (newVal) {
+    this.memeService.imgRequest().subscribe(() => console.log(newVal), () => this._imgSrc = this.memeService.notFoundImg);
+  }
+  @Input()
+  get aboveText () {
+    return this._aboveText;
+  }
+  set aboveText (newVal) {
+    this._aboveText = newVal;
+  }
+  @Input()
+  get belowText () {
+    return this._belowText;
+  }
+  set belowText (newVal) {
+    this._belowText = newVal;
+  }
 }
--- src/app/meme.service.ts
+++ src/app/meme.service.ts
@@ -8,11 +8,13 @@ export class MemeService {
   imgSrc: string;
   aboveText: string;
   belowText: string;
+  notFoundImg: string;
   constructor (private http: HttpClient) {}
   public loadData () {
     this.imgSrc = 'https://sdtimes.com/wp-content/uploads/2018/04/angular.png';
     this.aboveText = 'Above Text';
     this.belowText = 'Below Text';
+    this.notFoundImg = 'http://thetechtemple.com/wp-content/themes/TechNews/images/img_not_available.jpg';
   }
   public getData() {
     return {
